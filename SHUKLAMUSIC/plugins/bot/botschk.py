edc1e7362b32d11540ea35b605b8eebe27cfc011
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-09-08 13:52:17 +0530

    Update botschk.py

--- SHUKLAMUSIC/plugins/bot/botschk.py
+++ SHUKLAMUSIC/plugins/bot/botschk.py
@@ -26,7 +26,7 @@ from datetime import datetime
 userbot = Userbot()
 
 
-BOT_LIST = ["StrangerSuperbot", "Melaniarobot", "Itz_SapnaMusicbot", "ITZ_MERADHIKABOT", "StrangerHackBot"]
+BOT_LIST = ["vip_music_vc_bot"]
 
 @app.on_message(filters.command("botschk") & filters.user(OWNER_ID))
 async def bots_chk(_, message):
